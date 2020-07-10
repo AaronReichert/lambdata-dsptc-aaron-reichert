93967c960f77026fc08ad8a1310a75ed5ba28b24
AaronReichert <reichert.aaron.c@gmail.com>
2020-07-09 21:19:51 -0500

    Implimented train_validation_test_split function'

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # lambdata-dsptc-aaron-reichert
 A collection of data science utility functions.
+
+## instillation
+
+TODO
+
+## Usage
+
+TODO
--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_wine
+from pdb import set_trace as breakpoint
 
 def tvt_split(df, tvt_stratify='target', tvt_train_size=0.70, tvt_val_size=0.15, tvt_test_size=0.15, tvt_random_state=42):
   ''' first copy
@@ -22,3 +24,26 @@ def tvt_split(df, tvt_stratify='target', tvt_train_size=0.70, tvt_val_size=0.15,
   y_test=test[tvt_stratify]
 
   return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+
+  def train_validation_test_split(X, y, train_size=0.7, val_size=0.1, test_size=0.2, random_state=None, shuffle=True):
+            
+    X_train_val, X_test, y_train_val, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state, shuffle=shuffle)
+    
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train_val, y_train_val, test_size=val_size/(train_size+val_size), 
+        random_state=random_state, shuffle=shuffle)
+    
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+if __name__ == '__main__':
+
+
+    raw_data = load_wine()
+    df = pd.DataFrame(data=raw_data['data'], columns=raw_data['feature_names'])
+    df['target'] = raw_data['target']
+    breakpoint()
+
+    print(df.shape)
