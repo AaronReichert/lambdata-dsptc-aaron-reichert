120a452b18c12ef5066ecc742eeb0909b767acb8
AaronReichert <reichert.aaron.c@gmail.com>
2020-07-16 21:18:07 -0500

    docker file updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata_dspt6_aaron_reichert", # Replace with your own username
-    version="0.0.1",
+    version="0.0.3",
     author="Aaron Reichert",
     author_email="author@example.com",
     description="A small example package",
